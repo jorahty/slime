ae8c9bd8e000d9c427d71d4c2e8148c6e2dc1778
James Tennant <jtennant@ucsc.edu>
2023-03-23 18:06:42 -0700

    Client can connect to server

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,9 +1,42 @@
-import { SafeAreaView, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, SafeAreaView } from 'react-native';
+import io from 'socket.io-client';
+
+const socketEndpoint = "http://192.168.0.149:3000";
+// const socketEndpoint = "https://replit-socketio-expo.jorahty.repl.co";
 
 export default function App() {
+  const [rand, setRand] = useState(0);
+  const [error, setError] = useState('');
+
+  useEffect(function didMount() {
+    const socket = io(socketEndpoint, {
+      transports: ["websocket"],
+    });
+
+    socket.on('update', n => {
+      setRand(n);
+    });
+
+    socket.on('connect_error', err => {
+      setError(err.message);
+    });
+
+    return function didUnmount() {
+      socket.close();
+      socket.disconnect();
+      socket.removeAllListeners();
+    };
+  }, [])
+
   return (
     <SafeAreaView>
-      <Text>So no dark mode?</Text>
+      <Text>
+        rand: {rand}
+      </Text>
+      <Text>
+        {error ? `error: ${error}` : ''}
+      </Text>
     </SafeAreaView>
   );
 }
