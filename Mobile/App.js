c3d890d9b82a7f345f7b1ea09112d510462d6676
James Tennant <jtennant@ucsc.edu>
2023-03-23 17:42:40 -0700

    Mobile client setup

---
+++ Mobile/App.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>So no dark mode?</Text>
+    </SafeAreaView>
+  );
+}
---
+++ Mobile/README.md
@@ -0,0 +1,12 @@
+# Mobile
+
+Mobile client
+
+## Usage
+
+To run the project, run one of the following npm commands:
+
+- npm run start
+- npm run android
+- npm run ios
+- npm run web
